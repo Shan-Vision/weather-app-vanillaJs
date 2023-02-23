f5b7f7aed82653f55d0d9088ad3491cccd70a53e
Sherzod Abdullaev <perfekt.vision2019@gmail.com>
2023-02-23 09:24:11 +0100

    added images and all other buttons

--- dist/js/dataFunctions.js
+++ dist/js/dataFunctions.js
@@ -1,3 +1,5 @@
+const WEATHER_API_KEY = "a8800eb37cfa12f0ddce31783220ef9c";
+
 
 export const setLocationObject = (locationObj, coordsObj) => {
 	const { lat, lon, name, unit } = coordsObj;
@@ -11,4 +13,25 @@ export const setLocationObject = (locationObj, coordsObj) => {
 
 export const getHomeLocation = () => {
 	return localStorage.getItem("defaultWeatherLocation");
+};
+
+export const getCoordsFromApi = async (entryText, units) => {
+	const regex = /^\d+$/g;
+	const flag = regex.test(entryText) ? "zip" : "q";
+	const url = `https://api.openweathermap.org/data/2.5/weather?${flag}=${entryText}&units=${units}&appid=${WEATHER_API_KEY}`;
+	const encodedUrl = encodeURI(url);
+	try {
+		const dataStream = await fetch(encodedUrl);
+		const jsonData = await dataStream.json();
+		console.log('jsonData', jsonData)
+		return jsonData;
+	} catch (err) {
+		console.error(err.stack);
+	}
+};
+
+export const cleanText = (text) => {
+	const regex = / {2,}/g;
+	const entryText = text.replaceAll(regex, " ").trim();
+	return entryText;
 };
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,7 @@
 import CurrentLocation from "./CurrentLocation.js";
-import { addSpinner, displayError } from "./domFunctions.js";
+import { addSpinner, displayApiError, displayError, setPlaceholderText, updateScreenReaderConfirmation } from "./domFunctions.js";
 
-import { getHomeLocation, setLocationObject } from "./dataFunctions.js";
+import { cleanText, getCoordsFromApi, getHomeLocation, setLocationObject } from "./dataFunctions.js";
 const currentLoc = new CurrentLocation();
 
 const initApp = () => {
@@ -10,7 +10,16 @@ const initApp = () => {
 	geoButtons.addEventListener("click", getGeoWeather);
 	const homeButton = document.getElementById("home");
 	homeButton.addEventListener("click", loadWeather);
+	const saveButton = document.getElementById("saveLocation");
+	saveButton.addEventListener("click", saveLocation);
+	const unitButton = document.getElementById("unit");
+	unitButton.addEventListener("click", setUnitPref);
+	const refreshButton = document.getElementById("refresh");
+	refreshButton.addEventListener("click", refreshWeather);
+	const locationEntry = document.getElementById("searchBar__form");
+	locationEntry.addEventListener("submit", submitNewLocation);
 	// set up
+	setPlaceholderText();
 
 	// load weather
 };
@@ -28,6 +37,11 @@ const getGeoWeather = (event) => {
 	if (!navigator.geolocation) return geoError();
 	console.log("navigator", navigator);
 	navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
+};
+
+const geoError = (errObj) => {
+	const errMsg = errObj ? errObj.message : "Geolocation not supported";
+	displayError(errMsg, errMsg);
 };
 
 const geoSuccess = (position) => {
@@ -73,6 +87,63 @@ const displayHomeLocationWeather = (home) => {
 	}
 };
 
+const saveLocation = () => {
+	if (currentLoc.getLat() && currentLoc.getLon()) {
+		const saveIcon = document.querySelector(".fa-save");
+		addSpinner(saveIcon);
+		const location = {
+			name: currentLoc.getName(),
+			lat: currentLoc.getLat(),
+			lon: currentLoc.getLon(),
+			unit: currentLoc.getUnit(),
+		};
+		localStorage.setItem("defaultWeatherLocation", JSON.stringify(location));
+		updateScreenReaderConfirmation(
+			`Save ${currentLoc.getName()} as home location.`
+		);
+	}
+};
+
+const setUnitPref = () => {
+	const unitIcon = document.querySelector(".fa-chart-bar");
+	addSpinner(unitIcon);
+	currentLoc.toogleUnit();
+	updateDataAndDisplay(currentLoc);
+};
+
+const refreshWeather = () => {
+	const refreshIcon = document.querySelector(".fa-sync-alt");
+	addSpinner(refreshIcon);
+	updateDataAndDisplay(currentLoc);
+};
+
+const submitNewLocation = async (event) => {
+	event.preventDefault();
+	const text = document.getElementById("searchBar__text").value;
+	const entryText = cleanText(text);
+	if (!entryText.length) return;
+	const locationIcon = document.querySelector(".fa-solid");
+	addSpinner(locationIcon);
+	const coordsData = await getCoordsFromApi(entryText, currentLoc.getUnit());
+	
+	if (coordsData) {
+		if (coordsData.cod === 200) {
+			// work with api data
+			// success
+			const myCoordsObj = {
+				lat: coordsData.coord.lat,
+				lon: coordsData.coord.lon,
+				name: coordsData.name,
+			};
+			setLocationObject(currentLoc, myCoordsObj);
+			updateDataAndDisplay(currentLoc);
+		} else {
+			displayApiError (coordsData);
+		}
+	} else {
+		displayError("Connection error", "Connection error");
+	}
+};
 
 const updateDataAndDisplay = async (locationObj) => {
 	// const weatherJson = await getWeatherFromCoords(locationObj);
