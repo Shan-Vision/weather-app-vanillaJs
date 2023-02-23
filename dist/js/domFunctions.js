36d146eda999e9db6c0e9c1994781801a54e295b
Sherzod Abdullaev <perfekt.vision2019@gmail.com>
2023-02-23 11:36:30 +0100

    added display updating  dinamicly using  api data

--- dist/js/domFunctions.js
+++ dist/js/domFunctions.js
@@ -46,3 +46,81 @@ const toProperCase = (text) => {
 	});
 	return properWords.join(" ");
 };
+
+export const updateDisplay = (weatherJson, locationObj) => {
+	fadeDisplay();
+	clearDisplay();
+	const weatherClass = getWeatherClass(weatherJson.current.weather[0].icon);
+	setBGImage(weatherClass);
+	const screenReaderWeather = buildScreenReaderWeather(weatherJson, locationObj);
+	updateScreenReaderConfirmation(screenReaderWeather);
+	updateWeatherLocationHeader(locationObj.getName());
+	// current conditions
+	const ccArray = createCurrentConditionsDivs(
+		weatherJson,
+		locationObj.getUnit()
+	);
+	displayCurrentConditions(ccArray);
+	// six day forecast
+	displaySixDayForecast(weatherJson);
+	setFocusOnSearch();
+	fadeDisplay();
+};
+
+const fadeDisplay = () => {
+	const cc = document.getElementById("currentForecast");
+	cc.classList.toggle("zero-vis");
+	cc.classList.toggle("fade-in");
+	const sixDay = document.getElementById("dailyForecast");
+	sixDay.classList.toggle("zero-vis");
+	sixDay.classList.toggle("fade-in");
+};
+
+const clearDisplay = () => {
+	const currentConditions = document.getElementById(
+		"currentForecast__conditions"
+	);
+	deleteContents(currentConditions);
+	const sixDayForecast = document.getElementById("dailyForecast__contents");
+	deleteContents(sixDayForecast);
+};
+
+const deleteContents = (parentElement) => {
+	let child = parentElement.lastElementChild;
+	while (child) {
+		parentElement.removeChild(child);
+		child = parentElement.lastElementChild;
+	}
+};
+
+const getWeatherClass = (icon) => {
+	const firstTwoChars = icon.slice(0, 2);
+	const lastChar = icon.slice(2);
+	const weatherLookup = {
+		"01": "clear sky",
+		"02": "few clouds",
+		"03": "scattered clouds",
+		"04": "broken clouds",
+		"09": "snow",
+		10: "rain",
+		11: "thunderstorm",
+		13: "snow",
+		50: "fog",
+	};
+	let weatherClass;
+	if (weatherLookup[firstTwoChars]) {
+		weatherClass = weatherLookup[firstTwoChars];
+	} else if (lastChar === "d") {
+		weatherClass = "clouds";
+	} else {
+		weatherClass = "night";
+	}
+	return weatherClass;
+};
+
+const setBGImage = (weatherClass) => {
+	document.documentElement.classList.add(weatherClass);
+	document.documentElement.classList.forEach((img) => {
+		if (img !== weatherClass) document.documentElement.classList.remove(img);
+	});
+};
